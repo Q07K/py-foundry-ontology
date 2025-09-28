ec9476c6dc1cf313e2479afb45e704a09eb76999
gyuhyeong <kgh0730@gmail.com>
2025-09-28 16:28:12 +0900

    Feat: config.py 파일 추가 및 설정 클래스 정의
    - neo4j 및 sqlite 데이터베이스 설정을 위한 Settings 클래스 추가
    - 환경 변수 파일(.env) 및 인코딩 설정 구성
    - 캐시 기능을 활용한 get_settings 함수 정의

---
+++ src/core/config.py
@@ -0,0 +1,24 @@
+from functools import cache
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    # neo4j
+    neo4j_url: str
+    neo4j_username: str
+    neo4j_password: str
+
+    # sqlite
+    database_path: str
+
+    # Settings Config
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+    )
+
+
+@cache
+def get_settings() -> Settings:
+    return Settings()
