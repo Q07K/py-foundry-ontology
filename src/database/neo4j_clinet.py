8b482cef85c4fe4d0ec647d35e0cc172bc86733c
gyuhyeong <kgh0730@gmail.com>
2025-09-28 16:30:27 +0900

    Feat: neo4j 클라이언트 연결 및 해제 기능 추가
    - Neo4j 드라이버 연결을 위한 connection 함수 정의
    - Neo4j 드라이버 연결 해제를 위한 disconnect 함수 정의
    - 설정 값을 가져오기 위해 get_settings 함수 사용

---
+++ src/database/neo4j_clinet.py
@@ -0,0 +1,30 @@
+from neo4j import Driver, GraphDatabase
+
+from src.core.config import get_settings
+
+
+def connection() -> Driver:
+    """Neo4j 드라이버 연결
+
+    Returns
+    -------
+    Driver
+        Neo4j 드라이버 인스턴스
+    """
+    settings = get_settings()
+    return GraphDatabase.driver(
+        uri=settings.neo4j_url,
+        auth=(settings.neo4j_username, settings.neo4j_password),
+    )
+
+
+def disconnect(driver: Driver) -> None:
+    """Neo4j 드라이버 연결 해제
+
+    Parameters
+    ----------
+    driver : Driver
+        Neo4j 드라이버 인스턴스
+    """
+
+    driver.close()
